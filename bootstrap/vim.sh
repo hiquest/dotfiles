66102e3b32c7c168aee200cd34c923b9fa9e1114
Yanis <janis.sci@gmail.com>
2015-02-15 11:19:50 +0400

    Bootstrap vim

---
+++ bootstrap/vim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Installing pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# We will need those dirs
+mkdir -p ~/.vim/{backups,swaps,undo}
+
+# Saving old version of .vimrc
+if [ -f ~/.vimrc ]; then
+  mv ~/.vimrc ~/.vimrc.old
+fi
+
+# Copy .vimrc
+cp ../.vimrc ~/
+
+# Installing plugins
+cat ~/../vim.plugins | while read l; do cd ~/.git/bundle/ && git clone $l ; done
+
