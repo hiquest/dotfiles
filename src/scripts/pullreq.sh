1d50e76e6e0001efdca43549bfe9f62019c9c7d2
Yanis <janis.sci@gmail.com>
2015-05-11 15:39:07 +0400

    Some updates

---
+++ src/scripts/pullreq.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+project=`git remote -v | grep origin | head -n 1 | awk '{print $2}' | sed 's/.*github.com.//' | sed 's/\.git//'`
+branch=`git rev-parse --abbrev-ref HEAD`
+url="https://github.com/$project/compare/$branch?expand=1"
+open $url
