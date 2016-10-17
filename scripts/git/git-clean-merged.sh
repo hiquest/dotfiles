855fe9be6a9a2a4b2e3878e96aa1623b4d08e204
Yanis <janis.sci@gmail.com>
2016-10-17 09:22:44 +0400

    Update clean git script

--- scripts/git/git-clean-merged.sh
+++ scripts/git/git-clean-merged.sh
@@ -6,4 +6,4 @@ set -e
 
 git checkout master
 git pull --prune
-git branch --merged | grep -v "\*" | grep "yt-" | xargs -n 1 git branch -d
+git branch --merged | grep -v "master\|staging\|production\|dev" | xargs -n 1 git branch -d
