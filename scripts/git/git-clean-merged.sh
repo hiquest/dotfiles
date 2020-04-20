866d82a7b55b44b574c68b2db75c534e48ce30d5
Ianis <janis.sci@gmail.com>
2020-04-20 12:00:48 +0200

    Remove git scripts

--- scripts/git/git-clean-merged.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Removes merged local branches
-
-set -e
-
-git checkout master
-git pull --prune
-git branch --merged | grep -v "master\|staging\|production\|dev" | xargs -n 1 git branch -d
--- scripts/git/git-commit-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add and commit all files
-
-set -e
-
-git add . && git commit -m $@
